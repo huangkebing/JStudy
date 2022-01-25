53cf968427574bf5f946273c1f4755db397c55c1
huangkebing <1216033824@qq.com>
2022-01-25 10:43:13 +0800

    function-consumer

--- JDKLearn/src/test/com/hkb/FunctionTest.java
+++ JDKLearn/src/test/com/hkb/FunctionTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class FunctionTest {
     @Test
@@ -20,10 +23,7 @@ public class FunctionTest {
             int a = x + 4;
             System.out.println(a);
         }));
-    }
 
-    @Test
-    public void biConsumerTest(){
         // BiConsumer.andThen()
         HashMap<String, String> map = new HashMap<>();
         map.put("1","aaa");
@@ -31,5 +31,29 @@ public class FunctionTest {
         map.put("3","ccc");
         map.forEach(((BiConsumer<String,String>)(k, v) -> System.out.println(k))
                 .andThen((k,v) -> System.out.println(v)));
+    }
+
+    @Test
+    public void predicateTest(){
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(5);
+        integers.add(6);
+        integers.add(7);
+        integers.add(8);
+        integers.add(9);
+        integers.add(10);
+        integers.add(11);
+        integers.add(12);
+
+        List<Integer> collect1 = integers.stream()
+                // Predicate.and() 配合filter实现 与操作，过滤出3和4的公倍数
+                .filter(((Predicate<Integer>) i -> i % 4 == 0).and(i-> i % 3 == 0))
+                .collect(Collectors.toList());
+        collect1.forEach(System.out::println);
+
+        List<Integer> collect2 = integers.stream()
+                .filter(((Predicate<Integer>) i -> i % 4 == 0).or(i-> i % 3 == 0))
+                .collect(Collectors.toList());
+        collect2.forEach(System.out::println);
     }
 }
