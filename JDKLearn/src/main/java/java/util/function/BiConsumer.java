fd21996e9a97bfafe8bc7ae3ebc99bade1d43049
huangkebing <1216033824@qq.com>
2022-01-25 10:13:51 +0800

    function-consumer

--- JDKLearn/src/main/java/java/util/function/BiConsumer.java
+++ JDKLearn/src/main/java/java/util/function/BiConsumer.java
@@ -3,19 +3,7 @@ package java.util.function;
 import java.util.Objects;
 
 /**
- * Represents an operation that accepts two input arguments and returns no
- * result.  This is the two-arity specialization of {@link Consumer}.
- * Unlike most other functional interfaces, {@code BiConsumer} is expected
- * to operate via side-effects.
- *
- * <p>This is a <a href="package-summary.html">functional interface</a>
- * whose functional method is {@link #accept(Object, Object)}.
- *
- * @param <T> the type of the first argument to the operation
- * @param <U> the type of the second argument to the operation
- *
- * @see Consumer
- * @since 1.8
+ * 两个元素的消费者接口，HashMap.foreach()中使用了此接口
  */
 @FunctionalInterface
 public interface BiConsumer<T, U> {
