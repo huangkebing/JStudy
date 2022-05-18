4feae72fc7e0612e7b0da92807193db75700fd9e
huangkebing <1216033824@qq.com>
2022-05-18 10:50:26 +0800

    元注解

--- JDKLearn/src/main/java/java/lang/annotation/Repeatable.java
+++ JDKLearn/src/main/java/java/lang/annotation/Repeatable.java
@@ -1,49 +1,16 @@
-/*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
-
 package java.lang.annotation;
 
 /**
- * The annotation type {@code java.lang.annotation.Repeatable} is
- * used to indicate that the annotation type whose declaration it
- * (meta-)annotates is <em>repeatable</em>. The value of
- * {@code @Repeatable} indicates the <em>containing annotation
- * type</em> for the repeatable annotation type.
- *
- * @since 1.8
- * @jls 9.6 Annotation Types
- * @jls 9.7 Annotations
+ * 注解是用于声明其它类型注解的元注解，来表示这个声明的注解是可重复的。@Repeatable的值是另一个注解
+ * 其可以通过这个另一个注解的值来包含这个可重复的注解
+ * 1.8新增
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface Repeatable {
     /**
-     * Indicates the <em>containing annotation type</em> for the
-     * repeatable annotation type.
-     * @return the containing annotation type
+     * value是一个注解
      */
     Class<? extends Annotation> value();
 }
