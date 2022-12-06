16bf94624903b2c9068b130b7b3e282e3983fb6d
huangkebing <1216033824@qq.com>
2022-12-06 10:21:59 +0800

    移除进度记录

--- JDKLearn/src/main/java/com/kebing/process/java/util/concurrect/RejectedExecutionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.kebing.process.java.util.concurrect;
-
-import com.kebing.process.FinishedReading;
-
-@FinishedReading(date = "2022-10-02")
-public interface RejectedExecutionHandler {
-}
--- JDKLearn/src/main/java/com/kebing/process/java/util/concurrect/TimeUnit.java
+++
@@ -1,7 +0,0 @@
-package com.kebing.process.java.util.concurrect;
-
-import com.kebing.process.FinishedReading;
-
-@FinishedReading(date = "2022-08-15")
-public enum TimeUnit {
-}
