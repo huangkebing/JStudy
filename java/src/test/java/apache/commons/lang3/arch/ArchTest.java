ca6264666870489a8b81dc0f059cf120549f0055
huangkebing <1216033824@qq.com>
2023-04-06 14:40:05 +0800

    apache-commons-lang3 arch

--- java/src/test/java/apache/commons/lang3/arch/ArchTest.java
+++ java/src/test/java/apache/commons/lang3/arch/ArchTest.java
@@ -4,16 +4,28 @@ import org.apache.commons.lang3.ArchUtils;
 import org.apache.commons.lang3.arch.Processor;
 import org.junit.Test;
 
+/**
+ * apache.commons.lang3.arch.ArchUtils类使用示例
+ * 一些获取CPU信息的方法
+ */
 public class ArchTest {
     @Test
-    public void test() {
-        Processor processor = ArchUtils.getProcessor("amd64");
-        System.out.println(processor.getArch());
-        System.out.println(processor.getType());
-        System.out.println(processor.is32Bit());
-        System.out.println(processor.is64Bit());
-        System.out.println(processor.isIA64());
-        System.out.println(processor.isPPC());
-        System.out.println(processor.isX86());
+    public void archTest() {
+        // 获取本机的CPU信息
+        Processor equip = ArchUtils.getProcessor();
+        // 获取指定CPU信息
+        Processor x86 = ArchUtils.getProcessor("x86");
+        Processor ppc64 = ArchUtils.getProcessor("ppc64");
+        // 获取CPU的位数
+        System.out.println(ppc64.getArch());
+        // 获取CPU的类型
+        System.out.println(ppc64.getType());
+        // 判断CPU是否为32位/64位
+        System.out.println(ppc64.is32Bit());
+        System.out.println(ppc64.is64Bit());
+        // 判断CPU是否为IA64/PPC/X86
+        System.out.println(ppc64.isIA64());
+        System.out.println(ppc64.isPPC());
+        System.out.println(ppc64.isX86());
     }
 }
