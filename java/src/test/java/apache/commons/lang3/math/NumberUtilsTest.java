48f09f8ad943d81903ff38e9668072b75a47619d
huangkebing <1216033824@qq.com>
2023-04-07 19:51:29 +0800

    apache-commons-lang3 math

---
+++ java/src/test/java/apache/commons/lang3/math/NumberUtilsTest.java
@@ -0,0 +1,11 @@
+package apache.commons.lang3.math;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.junit.Test;
+
+public class NumberUtilsTest {
+    @Test
+    public void isNumberTest() {
+
+    }
+}
