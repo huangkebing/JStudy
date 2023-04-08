a0a0b6ab3c0c621aded4033daa36f167904ef001
huangkebing <1216033824@qq.com>
2023-04-08 15:22:46 +0800

    apache-commons-lang3 math.numbers

--- java/src/test/java/apache/commons/lang3/math/NumberUtilsTest.java
+++ java/src/test/java/apache/commons/lang3/math/NumberUtilsTest.java
@@ -6,6 +6,11 @@ import org.junit.Test;
 public class NumberUtilsTest {
     @Test
     public void isNumberTest() {
-
+        System.out.println(NumberUtils.isParsable("3.14"));
+        System.out.println(NumberUtils.isParsable("3"));
+        System.out.println(NumberUtils.isDigits("3.14"));
+        System.out.println(NumberUtils.isDigits("3"));
+        System.out.println(NumberUtils.isCreatable("3.14"));
+        System.out.println(NumberUtils.isCreatable("3"));
     }
 }
