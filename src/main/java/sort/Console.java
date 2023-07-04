46a40f062c0840355e284ea1701a807b5256caa3
huangkebing <1216033824@qq.com>
2023-07-04 10:30:11 +0800

    排序算法-归并排序

--- src/main/java/sort/Console.java
+++ src/main/java/sort/Console.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Console {
     public static void main(String[] args) {
         int[] array = {1,2,3,8,4,5,6,7};
-        System.out.println(Arrays.toString(Insertion.insertionSort(array)));
+        Merge.mergeSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
     }
 }
